62f73df20588a099b7700a9ae974b40ebb4ec04e
preox <preox@kth.se>
2015-07-06 19:47:03 +0200

    Initial Commit
    mother fuckin' pyramids

---
+++ SLApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SLApp
+{
+    static class Program
+    {
+        /*
+         * https://www.trafiklab.se/api/sl-platsuppslag
+         * 
+         * https://www.trafiklab.se/api/sl-realtidsinformation-3
+         * 
+         * 
+         *  */
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
